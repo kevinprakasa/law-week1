638f88f6a4e37d4150f7b1ee77b92827366de799
Kevin Prakasa <kevin.intern@stoqo.com>
2020-02-12 13:06:25 +0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+var rp = require("request-promise");
+
+var options = {
+  uri: "https://jobs.github.com/positions.json?location=new+york",
+  json: true // Automatically parses the JSON string in the response
+};
+
+router.get("/", function(req, res, next) {
+  rp(options)
+    .then(function(jobs) {
+      console.log("User has %d jobs", jobs.length);
+      res.render("index", {
+        title: "LAW Kevin Prakasa, Week 1",
+        jobs: jobs
+      });
+    })
+    .catch(function(err) {
+      res.render("index", { title: "Error" });
+      // API call failed...
+    }); /* GET home page. */
+});
+
+module.exports = router;
